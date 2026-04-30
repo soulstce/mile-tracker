11bc7973697a68939feb3e48e2cf31903ef22d9b
soulstce <soulstce000@gmail.com>
2026-04-30 01:52:52 -0600

    Cache versioned icon assets

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'mile-tracker-shell-v1';
-const APP_SHELL = ['/', '/manifest.json', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png', '/favicon.svg'];
+const APP_SHELL = ['/', '/manifest.json', '/icon-192-v2.png', '/icon-512-v2.png', '/apple-touch-icon-v2.png', '/favicon.svg'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
